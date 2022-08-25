d20d0f0674b70923f5c562b4250e5e7150264673
Adriana <adrianam754@gmail.com>
2022-08-25 15:01:12 -0300

    feat: add exercise  circle_radius

---
+++ java_exercise/src/circle_radius.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class circle_radius {
+  public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    double r , a , pi = 3.14159;
+
+    r = sc.nextDouble();
+
+    a = pi * r * r;
+
+    System.out.printf("a = %.4f%n", a);
+
+    sc.close();
+  }
+}
