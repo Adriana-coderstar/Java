944627b29e33c36c379d2ee09edc397a4e936211
Adriana <adrianam754@gmail.com>
2022-08-25 16:33:22 -0300

    feat: add exercise 6 calculate_parts

---
+++ java_exercise/src/calculate_parts.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class calculate_parts {
+  public static void main(String[] arg) {
+
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    int codeOne, quantPartsOne, codeTwo, quantPartsTwo;
+
+    double princeOne, princeTwo, totalPrice;
+
+    codeOne = sc.nextInt();
+    quantPartsOne = sc.nextInt();
+    princeOne = sc.nextDouble();
+    
+    codeTwo = sc.nextInt();
+    quantPartsTwo = sc.nextInt();
+    princeTwo = sc.nextDouble();
+
+    totalPrice = princeOne * quantPartsOne + princeTwo * quantPartsTwo;
+
+    System.out.printf("Valor a pagar: $ %.2f%n", totalPrice);
+
+    sc.close();
+  }
+}
