a8658e53921c94a86cd90364cad4fb194f26eeb6
Adriana <adrianam754@gmail.com>
2022-08-25 15:48:51 -0300

    feat: add exercise 5 salary

---
+++ java_exercise/src/salary.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class salary {
+  public static void main(String[] arg) {
+
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    int func, horasTrabalho;
+    double valorHora, sal;
+
+    func = sc.nextInt();
+
+    horasTrabalho = sc.nextInt();
+    valorHora = sc.nextDouble();
+
+    sal = valorHora * horasTrabalho;
+
+    System.out.println("Número do funcionário: " + func);
+    System.out.printf("Salário recebido: U$ %.2f%n", sal);
+
+    sc.close();
+  }
+}
