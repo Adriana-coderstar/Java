708852c968d90cef6052c9386c570c3ba3d07dd0
Adriana <adrianam754@gmail.com>
2022-08-25 15:16:26 -0300

    feat: add exercise 4 different_values

---
+++ java_exercise/src/different_values.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class different_values {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a, b, c, d, dif;
+
+    a = sc.nextInt();
+    b = sc.nextInt();
+    c = sc.nextInt();
+    d = sc.nextInt();
+
+    dif = a * b - c * d;
+
+    System.out.println("A diferença é =" + dif);
+
+    sc.close();
+  }
+}
