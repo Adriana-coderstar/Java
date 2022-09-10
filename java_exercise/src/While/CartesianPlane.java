86d1bf17ffc4e254c73463ba84db1e8bffbbbf17
Adriana <adrianam754@gmail.com>
2022-09-10 17:39:20 -0300

    feat: add exercise with While CartesianPlane

---
+++ java_exercise/src/While/CartesianPlane.java
@@ -0,0 +1,27 @@
+package While;
+import java.util.Scanner;
+
+public class CartesianPlane {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int x = sc.nextInt();
+    int y = sc.nextInt();
+
+    while( x != 0 && y != 0 ){
+      if (x > 0 && y > 0) {
+        System.out.println("Primeiro");
+      } else if (x < 0 && y > 0) {
+        System.out.println("Segundo");
+      } else if (x < 0 && y < 0 ) {
+        System.out.println("Terceiro");
+      } else {
+        System.out.println("Quarto");
+      }
+      x = sc.nextInt();
+      y = sc.nextInt();
+    }
+
+    sc.close();
+  }
+}
